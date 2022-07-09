28d0ee41a0d0ea466e76ee28fb4d7fda1162da82
sangnt1552314 <sang1552314@gmail.com>
2022-07-09 23:00:06 +0700

    init layout

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import time
+
+st.title('Bank Telemarketing Prediction')
+
+st.markdown(
+    """
+    Simple application of bank telemarketing for classification customer
+    """
+)
+
+st.sidebar.header("User Input Features")
+with st.sidebar:
+    st.text('This is some text.')
+
